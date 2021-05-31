ba708360fa719699c674e66494c1c25d54741288
yanyunwu <2480721346@qq.com>
2021-05-31 08:27:57 +0800

    注册模块

--- src/views/register.vue
+++ src/views/register.vue
@@ -12,11 +12,22 @@
       <el-form-item label="用户名" prop="username">
         <el-input v-model="form.username" placeholder="8~16个字符"></el-input>
       </el-form-item>
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" placeholder="8~16个字符"></el-input>
+      <el-form-item label="性别" prop="sex">
+        <el-radio-group v-model="form.sex">
+          <el-radio-button label="f">男</el-radio-button>
+          <el-radio-button label="m">女</el-radio-button>
+          <el-radio-button label="s">保密</el-radio-button>
+        </el-radio-group>
       </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="form.password" placeholder="6~16个字符"></el-input>
+      <el-form-item label="生日" prop="birthday">
+        <el-date-picker
+          v-model="form.birthday"
+          type="date"
+          placeholder="选择日期"
+          format="yyyy年MM月dd日"
+          value-format="yyyy MM dd"
+        >
+        </el-date-picker>
       </el-form-item>
       <el-form-item label="密码" prop="password">
         <el-input v-model="form.password" placeholder="6~16个字符"></el-input>
@@ -24,6 +35,7 @@
       <el-form-item>
         <el-button native-type="submit" type="primary">立即注册</el-button>
         <el-button @click="$emit('register')">马上登录</el-button>
+        <el-button @click="clear">清空</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -35,6 +47,8 @@ export default {
     return {
       form: {
         username: "",
+        sex: "",
+        birthday: "",
         password: "",
       },
       rules: {
@@ -42,6 +56,8 @@ export default {
           { required: true, message: "用户名不能为空", trigger: "blur" },
           { required: true, message: "", trigger: "blur" },
         ],
+        sex: [{ required: true, message: "请选择性别", trigger: "blur" }],
+        birthday: [{ required: true, message: "请选择日期", trigger: "blur" }],
         password: [
           { required: true, message: "密码不能为空", trigger: "blur" },
         ],
@@ -53,10 +69,17 @@ export default {
       this.$refs["form"].validate((valid) => {
         if (valid) {
           console.log("成功");
+          console.log(this.form);
         } else {
           console.log("校验失败");
         }
       });
+    },
+    clear() {
+      let i = undefined;
+      for (i in this.form) {
+        this.form[i] = "";
+      }
     },
   },
   created() {
